12057e42be17779ca3df9c51521a381c9c67d25c
Mohammad Mohabbati <Mohammad.Mohabbati@rhenus.com>
2025-02-03 10:10:40 +0100

    Add project files.

---
+++ LearnSemanticKernel/FilePlugin.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.SemanticKernel;
+
+namespace LearnSemanticKernel;
+
+public class FilePlugin
+{
+    public string RootFolder { get; set; }
+
+    public FilePlugin()
+    {
+        RootFolder = @"c:\sk-temp";
+        if (!Directory.Exists(RootFolder))
+        {
+            Directory.CreateDirectory(RootFolder);
+        }
+    }
+
+    [KernelFunction("get_root_folder")]
+    public string GetRootFolder()
+    {
+        return RootFolder;
+    }
+
+    [KernelFunction("create_folder")]
+    public void CreateFolder(string folderPath)
+    {
+        Guard(folderPath);
+        Directory.CreateDirectory(folderPath);
+    }
+
+    [KernelFunction("create_file")]
+    public void CreateFile(string filePath, string content)
+    {
+        Guard(filePath);
+        File.WriteAllText(filePath, content);
+    }
+
+    [KernelFunction("get_content_of_file")]
+    public string GetContentOfFile(string filePath)
+    {
+        Guard(filePath);
+        return File.ReadAllText(filePath);
+    }
+
+    [KernelFunction("move_file")]
+    public void MoveFile(string sourceFilePath, string targetFilePath)
+    {
+        Guard(sourceFilePath);
+        Guard(targetFilePath);
+        File.Move(sourceFilePath, targetFilePath);
+    }
+
+    [KernelFunction("delete_file")]
+    public void DeleteFile(string filePath)
+    {
+        Guard(filePath);
+        File.Delete(filePath);
+    }
+
+    [KernelFunction("move_folder")]
+    public void MoveFolder(string sourceFolderPath, string targetFolderPath)
+    {
+        Guard(sourceFolderPath);
+        Guard(targetFolderPath);
+        Directory.Move(sourceFolderPath, targetFolderPath);
+    }
+
+    [KernelFunction("delete_folder")]
+    public void DeleteFolder(string folderPath)
+    {
+        Guard(folderPath);
+        Directory.Delete(folderPath);
+    }
+
+    [KernelFunction("get_files_for_folder")]
+    public string[] GetFiles(string folderPath)
+    {
+        Guard(folderPath);
+        return Directory.GetFiles(folderPath);
+    }
+
+    [KernelFunction("get_folders_for_folder")]
+    public string[] GetFolders(string folderPath)
+    {
+        Guard(folderPath);
+        return Directory.GetDirectories(folderPath);
+    }
+
+    private void Guard(string folderPath)
+    {
+        if (!folderPath.StartsWith(RootFolder))
+        {
+            throw new Exception("No you don't!");
+        }
+    }
+}
---
+++ LearnSemanticKernel/Program.cs
@@ -0,0 +1,94 @@
+﻿#region warning disable
+//#pragma warning disable SKEXP0001
+#pragma warning disable SKEXP0110
+#pragma warning disable SKEXP0050
+#endregion
+
+using LearnSemanticKernel;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel.Agents;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.AzureOpenAI;
+using Microsoft.SemanticKernel.Plugins.Core;
+using Microsoft.SemanticKernel;
+using System.Text;
+
+Console.WriteLine("Hello, how can I assist you!");
+
+var configuration = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+
+var builder = Kernel.CreateBuilder();
+
+builder.AddAzureOpenAIChatCompletion("gpt-4o-mini", configuration.GetValue<string>("Endpoint")!, configuration.GetValue<string>("ApiKey")!);
+
+builder.Services.AddSingleton<IAutoFunctionInvocationFilter, AutoInvocationFilter>();
+
+var kernel = builder.Build();
+
+var history = new ChatHistory();
+
+kernel.ImportPluginFromType<TimePlugin>();
+
+#region Plugin
+var firstPlugin = new FilePlugin();
+kernel.ImportPluginFromObject(firstPlugin);
+#endregion
+
+var agent = new ChatCompletionAgent
+{
+    Name = "MyAgent",
+    Kernel = kernel,
+    //Instructions = "You are a basic assistant. Response in German.",
+
+    #region Plugin
+    Instructions = $"Your name is SK Ninja. You are File Manager that can create and list files and folders. " +
+                   $"When you create files and folder you need to give the full path based on this root folder: {firstPlugin.RootFolder}",
+
+    Arguments = new KernelArguments(
+        new AzureOpenAIPromptExecutionSettings
+        {
+            Temperature = 0.5,
+            FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+        })
+    #endregion
+};
+
+Console.OutputEncoding = Encoding.UTF8;
+while (true)
+{
+    Console.Write("You > ");
+    var question = Console.ReadLine() ?? "";
+    history.AddUserMessage(question);
+
+    try
+    {
+        Console.Write("Assistant : ");
+
+        await foreach (var response in agent.InvokeStreamingAsync(history))
+        {
+            foreach (var content in response.Content ?? "")
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write(content);
+            }
+        }
+    }
+    catch (Exception e)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine("Exception: " + e.Message);
+    }
+    finally
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    Console.WriteLine();
+    Console.ForegroundColor = ConsoleColor.DarkGray;
+    Console.WriteLine("------------------------------------------------------");
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine();
+}
